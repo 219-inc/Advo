ff4baa2196b2238a6b82b16f63aad69d1a381a7f
Divyansh Gupta <divyansh@rysyl.com>
2022-09-04 09:52:20 +0530

    App icons and splashscreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { View, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import tw from "twrnc";
 import { Amplify } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
@@ -7,7 +7,7 @@ import awsconfig from "./src/aws-exports";
 import { LocationProvider } from "context/LocationContext";
 import { NotificationBoundary } from "./src/layouts/NotificationBoundary";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import SplashScreen from "component/SplashScreen";
+import SplashScreen from "screen/SplashScreen";
 
 Amplify.configure(awsconfig);
 
