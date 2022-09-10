8225f46595e062b472dfc5ef6c4227145059c817
Divyansh Gupta <divyansh@rysyl.com>
2022-09-11 01:32:05 +0530

    .

--- amplify/backend/function/AdvoGetCurrentAuthUser/src/index.js
+++ amplify/backend/function/AdvoGetCurrentAuthUser/src/index.js
@@ -3,11 +3,16 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const queryExec = require('./QueryExec')
+
 exports.handler = async (event) => {
     
     let userId = event.requestContext.authorizer?.jwt.claims.sub
 
-    let user = await QueryExec(`SELECT * FROM users WHERE userId = '${userId}'`)
+    let user = await queryExec(
+      `SELECT * FROM users WHERE userId = '${userId}'`
+    );
 
     return {
         statusCode: 200,
